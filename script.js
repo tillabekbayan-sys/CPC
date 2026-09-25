9487496afaab8f0cb1e215fe460dde46ff990073
tillabekbayan-sys <tillabekbayan@gmail.com>
2026-09-25 10:53:50 +0500

    Add button click event to display message

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const button = document.getElementById("myButton");
+const message = document.getElementById("message");
 
+button.addEventListener("click", function() {
+    message.textContent = "Бұл сайт HTML, CSS және JavaScript көмегімен жасалды!";
+});
